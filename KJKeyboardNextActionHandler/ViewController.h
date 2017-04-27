e0178b4315c808de9bbcf1ef0f5ce846ee4d2519
mm <mm@mmdemac-mini.local>
2017-04-27 15:16:27 -0700

    Initial Commit

---
+++ KJKeyboardNextActionHandler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KJKeyboardNextActionHandler
+//
+//  Created by mm on 17/4/27.
+//  Copyright © 2017年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KJKeyboardNextActionHandlerTests/KJKeyboardNextActionHandlerTests.m
@@ -0,0 +1,39 @@
+//
+//  KJKeyboardNextActionHandlerTests.m
+//  KJKeyboardNextActionHandlerTests
+//
+//  Created by mm on 17/4/27.
+//  Copyright © 2017年 mm. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KJKeyboardNextActionHandlerTests : XCTestCase
+
+@end
+
+@implementation KJKeyboardNextActionHandlerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
